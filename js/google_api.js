f2c33160150a1f0d7bdb0b3332b71d5a76eaeb6a
rabbit159t <rabbit159t@gmail.com>
2019-06-18 23:34:50 +0800

    split js file

---
+++ js/google_api.js
@@ -0,0 +1,112 @@
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    }, function(error) {
+        appendPre(JSON.stringify(error, null, 2));
+    });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        listMajors();
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+    }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+    var pre = document.getElementById('result');
+    var resultContent = document.createTextNode(message + '\n');
+    pre.appendChild(resultContent);
+}
+
+function cleanPre() {
+    var pre = document.getElementById('result');
+    while (pre.firstChild) {
+        pre.removeChild(pre.firstChild);
+    }
+}
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+function listMajors() {
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: githubURL.searchParams.get("spreadsheetId"),
+        range: 'Data!A2:D',
+    }).then(function(response) {
+        cleanPre();
+        var range = response.result;
+        if (range.values.length > 0) {
+            appendPre('會員編號, 資料建立時間, 類別, 內容');
+            for (i = 0; i < range.values.length; i++) {
+                var row = range.values[i];
+                // Print columns A and E, which correspond to indices 0 and 4.
+                if(searchSelect.value == "id"){
+                if(row[0] == searchContent.value){
+                    appendPre(row.join());
+                }
+                }
+                else{
+                if(searchSelect.value == row[2]){
+                    appendPre(row.join());
+                }
+                }
+            }
+        } else {
+            appendPre('No data found.');
+        }
+    }, function(response) {
+        appendPre('Error: ' + response.result.error.message);
+    });
+}
