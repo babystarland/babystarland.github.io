1623e1a36b9e84c0aade64070dc2f3086755342a
rabbit159t <rabbit159t@gmail.com>
2019-06-29 00:08:03 +0800

    use one API key

--- js/google_api.js
+++ js/google_api.js
@@ -40,6 +40,7 @@ function updateSigninStatus(isSignedIn) {
         searchContentDiv.style.display = 'block';
         searchButton.style.display = 'block';
         listMajors();
+        callScriptFunction();
     } else {
         authorizeButton.style.display = 'block';
         signoutButton.style.display = 'none';
@@ -59,6 +60,57 @@ function handleAuthClick(event) {
 function handleSignoutClick(event) {
     cleanPre();
     gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Calls an Apps Script function to get the data from the Google Sheet
+ */
+function callScriptFunction() {
+    var request = {
+        'function': 'getHistory'
+    };
+    // Make the API request.
+    var op = gapi.client.request({
+        'root': 'https://script.googleapis.com',
+        'path': 'v1/scripts/' + SCRIPT_ID + ':run',
+        'method': 'POST',
+        'body': request
+    });
+    op.execute(function(resp){ 
+        handleGetDataResponse(resp);
+    });
+}
+function handleGetDataResponse(resp) {
+    if (resp.error && resp.error.status) {
+        // The API encountered a problem before the script started executing.
+        appendPre('Error calling API:');
+        appendPre(JSON.stringify(resp, null, 2));
+    } else if (resp.error) {
+        // The API executed, but the script returned an error.
+        // The values of this object are the script's 'errorMessage' 'errorType',
+        // and an array of stack trace elements.
+        var error = resp.error.details[0];
+        appendPre('Script error message: ' + error.errorMessage);
+        if (error.scriptStackTraceElements) {
+            // There may not be a stacktrace if the script didn't start executing.
+            appendPre('Script error stacktrace:');
+            for (var i = 0; i < error.scriptStackTraceElements.length; i++) {
+                var trace = error.scriptStackTraceElements[i];
+                appendPre('\t' + trace.function+':' + trace.lineNumber);
+            }
+        }
+    } else {
+        // Apps Script function return. 
+        console.log(resp.response.result);
+        console.log(resp.response.result.headers);
+        records = resp.response.result.records;
+        headers = resp.response.result.headers;
+        if (Object.keys(records).length == 0) {
+            appendPre('No records returned!');
+        } else {
+            // TODO
+        }
+    }
 }
 
 /**
@@ -82,7 +134,6 @@ function cleanPre() {
 
 /**
  * Print the names and majors of students in a sample spreadsheet:
- * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  */
 function listMajors() {
     gapi.client.sheets.spreadsheets.values.get({
