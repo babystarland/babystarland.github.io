ddb9fd7e0cf58e0acb6a9bda850d044085fc1b2b
rabbit159t <rabbit159t@gmail.com>
2019-07-02 00:45:14 +0800

    show history

--- js/show.js
+++ js/show.js
@@ -1,3 +1,7 @@
+function isEmpty() {
+    if(searchContent.value.trim() === "") return true;
+}
+
 function appendPre(message) {
     var pre = document.getElementById('result');
     var resultContent = document.createTextNode(message + '\n');
@@ -9,11 +13,51 @@ function cleanPre() {
     while (pre.firstChild) {
         pre.removeChild(pre.firstChild);
     }
+    myRadarChart.destroy();
+}
+
+function showHistory(){
+    appendPre(babyHistory["headers"].join());
+    var search_type = searchSelect.value;
+    var idx_in_history = (search_type == "會員編號") ? babyHistory["headers"].indexOf("會員編號") : babyHistory["headers"].indexOf("內容");
+    var cat_idx_in_history = babyHistory["headers"].indexOf("類別");
+    chart_data["datasets"][0]["data"] = [0, 0, 0, 0, 0];
+    
+    for(var row in babyHistory["records"]){
+        if(babyHistory["records"][row][idx_in_history].indexOf(searchContent.value) !== -1){
+            appendPre(babyHistory["records"][row].join());
+
+            if(search_type == "會員編號"){
+                var cat_idx = CATEGORY.indexOf(babyHistory["records"][row][cat_idx_in_history])
+                chart_data["datasets"][0]["data"][cat_idx] += 1;
+
+                myRadarChart = new Chart(ctx, {
+                    type: 'radar',
+                    data: chart_data,
+                    options: {
+                        legend: {
+                            position: 'top',
+                        },
+                        scale: {
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+            }
+            else{}
+        }
+    }
 }
 
 function showResult() {
+    if(isEmpty()) return false;
     cleanPre();
+    var search_type = searchSelect.value;
+    var idx_in_history = babyHistory["headers"].indexOf(search_type);
     
+    // find match item in 0-6 sheet
     var cur = {};
     if(searchSelect.value == "會員編號"){
         for(var sheet_name in allData){
@@ -27,34 +71,7 @@ function showResult() {
                 }
             }
         }
-        
-        // processing history
-        appendPre(babyHistory["headers"].join());
-        chart_data["datasets"][0]["data"] = [0, 0, 0, 0, 0];
-        var id_idx = babyHistory["headers"].indexOf("會員編號");
-        for(var row in babyHistory["records"]){
-            if(babyHistory["records"][row][id_idx] == searchContent.value){
-                appendPre(babyHistory["records"][row].join());
-
-                var cat_idx_in_header = babyHistory["headers"].indexOf("類別");
-                var cat_idx = CATEGORY.indexOf(babyHistory["records"][row][cat_idx_in_header])
-                chart_data["datasets"][0]["data"][cat_idx] += 1;
-            }
-        }
-        myRadarChart = new Chart(ctx, {
-            type: 'radar',
-            data: chart_data,
-            options: {
-                legend: {
-                    position: 'top',
-                },
-                scale: {
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
     }
     else{}
+    showHistory();
 }
